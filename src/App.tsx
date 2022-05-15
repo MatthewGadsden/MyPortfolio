1a94035d20a8e1790e43ca418d6c0d740f109f92
mgad0002 <mgad0002@student.monash.edu>
2022-05-15 19:23:00 +1000

    adding themes and set up dark / light mode with material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import lightTheme from './context/themes/light-theme';
+import darkTheme from './context/themes/dark-theme';
 import './App.css';
 
 import MyNavbar from "./layouts/my-navbar";
@@ -10,17 +14,23 @@ import Resume from './pages/resume';
 import About from './pages/about';
 
 
+
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div>
-      <MyNavbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/portfolio" element={<Portfolio />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/resume" element={<Resume />}/>
-      </Routes>
-    </div>
+    <MuiThemeProvider theme={darkTheme}>
+      <StyledThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <MyNavbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/portfolio" element={<Portfolio />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/resume" element={<Resume />}/>
+        </Routes>
+      </StyledThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
