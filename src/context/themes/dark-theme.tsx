adec77e63769898dce31881baa06a09f1b281c5b
MatthewGadsden <matthewjgadsden+coding@gmail.com>
2022-05-20 21:22:25 +1000

    changes to dark theme

--- src/context/themes/dark-theme.tsx
+++ src/context/themes/dark-theme.tsx
@@ -4,7 +4,13 @@ import { createTheme } from '@material-ui/core';
 const darkTheme = createTheme({
   palette: {
     primary: {
-      main: '#010101'
+      main: '#1b1b1b',
+      light: '#464646',
+      dark: '#090909',
+      contrastText: '#fefefe',
+    },
+    secondary: {
+      // main: '#F50057'
     }
   },
   spacing: (factor) => `${0.1 * factor}rem`,
